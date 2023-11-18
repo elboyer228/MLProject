6e9580aaf91bbe3b7dd090efca9c9655ee54d092
Johann Clausen <johann.clausen@epfl.ch>
2023-11-18 18:13:50 +0100

    Adds permutation_importance computation

--- importance.py
+++ importance.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.inspection import permutation_importance
 import pandas as pd
 import numpy as np
 
@@ -11,7 +12,7 @@ X = df.drop(columns=['ID'])
 y = df_target['RT']
 
 # Train model
-model = RandomForestRegressor()
+model = RandomForestRegressor(random_state=42)
 model.fit(X, y)
 
 # Importance of all characteristics
@@ -23,3 +24,12 @@ feature_importances = {features[i]: importances[i] for i in range(len(features))
 df_importances = pd.DataFrame(list(feature_importances.items()), columns=['Feature', 'Importance'])
 df_importances = df_importances.sort_values('Importance', ascending=False)
 df_importances.to_csv("Features/importance.csv")
+
+
+# Also computes the permutation importance and saves it
+perm_importance = permutation_importance(model, X, y, n_repeats=10, random_state=42)
+
+perm_importances = {features[i]: perm_importance.importances_mean[i] for i in range(len(features))}
+df_perm_importances = pd.DataFrame(list(perm_importances.items()), columns=['Feature', 'Permutation Importance'])
+df_perm_importances = df_perm_importances.sort_values('Permutation Importance', ascending=False)
+df_perm_importances.to_csv("Features/permutation_importance.csv")
