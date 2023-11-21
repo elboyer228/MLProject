fae05bb139ed8722d5077e157db5a3c235782267
Johann Clausen <johann.clausen@epfl.ch>
2023-11-21 14:25:37 +0100

    Adding all code needed to reproducibility

--- linearRegression.py
+++ linearRegression.py
@@ -5,43 +5,68 @@ from tools import saveSubmission
 
 # Sklearn
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
 
-# Import the data
-train = pd.read_csv("Data/train.csv")
-test = pd.read_csv("Data/test.csv")
-train_properties = pd.read_csv("Features/train_properties.csv")
-test_properties = pd.read_csv("Features/test_properties.csv")
+def LinearRegressionModel(type = "normal"):
+    
+    # Import the data
+    train = pd.read_csv("Data/train.csv")
+    test = pd.read_csv("Data/test.csv")
+    train_properties = pd.read_csv("Features/train_properties.csv")
+    test_properties = pd.read_csv("Features/test_properties.csv")
+    
+    # Define features and target
+    normal = ["MolWt", "NumValenceElectrons", "MaxPartialCharge"]
+    enhanced = ["MolLogP", "BertzCT", "NumAliphaticHeterocycles"]
+    target = "RT"
+    
+    if type == "fingerprints":
+        X = train.loc[:, 'ECFP_1':'ECFP_1024']
+        X_test = test.loc[:, 'ECFP_1':'ECFP_1024']
+        y = train[target]
+    elif type == "normal":
+        X = train_properties.loc[:, normal]
+        X_test = test_properties.loc[:, normal]
+        y = train[target]
+    elif type == "enhanced":
+        X = train_properties.loc[:, enhanced]
+        X_test = test_properties.loc[:, enhanced]
+        y = train[target]
 
-target = "RT"
-features = ["MolLogP", "BertzCT", "NumAliphaticHeterocycles"]
+    # Defining model
+    model = LinearRegression()
+    model.fit(X, y)
 
-# Merging training data and computed features
-LightTrain = train[["Compound", "SMILES", "Lab", "RT"]]
-LightProp = train_properties[features]
-newTrain = pd.concat([LightTrain, LightProp], axis=1)
-
-LightTest = test[["Compound", "SMILES", "Lab"]]
-LightTProp = test_properties[features]
-newTest = pd.concat([LightTest, LightTProp], axis=1)
-
-# Defining model
-model = LinearRegression()
-model.fit(newTrain[features], newTrain[target])
-
-# Predicting and saving data
-predicted = model.predict(newTest[features])
-saveSubmission(predicted, "TargetLinearRegression")
+    # Predicting and saving data
+    predicted = model.predict(X_test)
+    saveSubmission(predicted, "LinearRegression_" + type)
 
 
-# Testing the fit by plotting predictions on the training data
-predictedTraining = model.predict(newTrain[features])
-plt.figure()
-plt.scatter(newTrain[target], predictedTraining, s=2, label="Linear Regression")
-x = np.linspace(min(newTrain[target]), max(newTrain[target]), 100) 
-plt.plot(x, x, color='black', linestyle='--') # Plot identity line
-plt.xlabel("true counts")
-plt.ylabel("predicted mode of counts")
-plt.legend()
-plt.show()
+    # Testing the fit by plotting predictions on the training data
+    predictedTraining = model.predict(X)
+    plt.figure()
+    plt.scatter(y, predictedTraining, s=2, label="Linear Regression")
+    x = np.linspace(min(y), max(y), 100) 
+    plt.plot(x, x, color='black', linestyle='--') # Plot identity line
+    plt.xlabel("true counts")
+    plt.ylabel("predicted mode of counts")
+    plt.legend()
+    plt.show()
+
+    # Computing metrics
+    mae = mean_absolute_error(y, predictedTraining)
+    mse = mean_squared_error(y, predictedTraining)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y, predictedTraining)
+
+    print(f"Linear Regression ({type})")
+    print(f"MAE: {mae}")
+    print(f"MSE: {mse}")
+    print(f"RMSE: {rmse}")
+    print(f"R2 Score: {r2}")
+    
+LinearRegressionModel("fingerprints")
+LinearRegressionModel("normal")
+LinearRegressionModel("enhanced")
