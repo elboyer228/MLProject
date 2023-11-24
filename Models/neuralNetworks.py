6177ec8bf91fdab060c8bb5d852319ede4a82e1e
Johann Clausen <johann.clausen@epfl.ch>
2023-11-24 14:19:32 +0100

    Trying early stopping based on validation set

--- Models/neuralNetworks.py
+++ Models/neuralNetworks.py
@@ -7,6 +7,7 @@ import numpy as np
 import random
 from tqdm import tqdm
 
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 import torch
@@ -35,21 +36,28 @@ train = pd.read_csv("Features/train_properties.csv")
 test = pd.read_csv("Features/test_properties.csv")
 target = pd.read_csv("Data/train.csv")
 
-# Define features and target
-X = train.drop(["ID"], axis=1)
-X_test = test.drop(["ID"], axis=1)
-y = target["RT"]
+# Splitting test and train
+
+X_train, X_val, y_train, y_val = train_test_split(
+    train.drop(["ID"], axis=1),
+    target["RT"],
+    test_size=0.2,
+    random_state=42
+)
 
 # Standardize data
-scaler = StandardScaler()
-X_std = scaler.fit_transform(X)
-X_test_std = scaler.transform(X_test)
-y_std = scaler.fit_transform(y.values.reshape(-1, 1))
+Xscaler = StandardScaler()
+X_std = Xscaler.fit_transform(X_train)
+X_val_std = Xscaler.transform(X_val)
+X_test_std = Xscaler.transform(test.drop(["ID"], axis=1))
+Yscaler = StandardScaler()
+y_std = Yscaler.fit_transform(y_train.values.reshape(-1, 1))
+y_val_std = Yscaler.transform(y_val.values.reshape(-1, 1))
 
 # Convert data to pytorch tensors
-X_ten, y_ten, X_test_ten = map(
+X_ten, y_ten, X_val_ten, y_val_ten, X_test_ten = map(
     lambda array: torch.tensor(array, dtype=torch.float32),
-    [X_std, y.values.reshape(-1, 1), X_test_std]
+    [X_std, y_std, X_val_std, y_val_std, X_test_std]
 )
 
 # Pytorch training data loader, for using batches
@@ -77,6 +85,11 @@ class NeuralNet(torch.nn.Module):
 batch_size = 128
 epochs = 150
 
+# Initialize best validation loss as infinity
+best_val_loss = float('inf')
+# Initialize patience counter
+patience_counter = 0
+
 model = NeuralNet()
 loss_func = torch.nn.MSELoss()
 optimizer = torch.optim.AdamW(model.parameters())
@@ -84,20 +97,52 @@ optimizer = torch.optim.AdamW(model.parameters())
 # Training
 model.train()
 for epoch in tqdm(range(epochs)):
+    model.train()
     for data, target in training_dataloader:
         optimizer.zero_grad()
         pred = model(data)
         loss = torch.sqrt(loss_func(pred, target))
         loss.backward()
         optimizer.step()
+        
 
-
-# Evaluation
-with torch.no_grad():
+    # Validation
     model.eval()
-    train_preds = model(X_ten).detach().numpy()
-    train_gt = y_ten.detach().numpy()
-    train_loss = np.sqrt(mean_squared_error(train_preds, train_gt))
+    with torch.no_grad():
+        pred_val = model(X_val_ten)  # assuming X_val_ten is your validation data
+        val_loss = torch.sqrt(loss_func(pred_val, y_val_ten))  # assuming y_val_ten is your validation target
+
+    # Check for early stopping
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        patience_counter = 0
+    else:
+        patience_counter += 1
+        if patience_counter >= 3:  # 5 is the patience
+            print("Early stopping")
+            break
+
+
+# Evaluation and computation of the train and test losss
+model.eval()
+with torch.no_grad():
+    pred_train = model(X_ten)
+    pred_val = model(X_val_ten)
+
+    # Inverse transform the predictions
+    pred_train = Yscaler.inverse_transform(pred_train.numpy())
+    pred_val = Yscaler.inverse_transform(pred_val.numpy())
+
+    # Convert the targets back to numpy and inverse transform
+    y_train_np = Yscaler.inverse_transform(y_ten.numpy())
+    y_val_np = Yscaler.inverse_transform(y_val_ten.numpy())
+
+    # Compute the loss with the inverse transformed data
+    train_loss = torch.sqrt(loss_func(torch.from_numpy(pred_train), torch.from_numpy(y_train_np)))
+    test_loss = torch.sqrt(loss_func(torch.from_numpy(pred_val), torch.from_numpy(y_val_np)))
+
+    print(f"Train loss: {train_loss}")
+    print(f"Validation loss: {test_loss}")
     
-    predictions = model(X_test_ten).detach().numpy().flatten()
-    saveSubmission(predictions, "NeuralNetwork_properties")
+    predictions = Yscaler.inverse_transform(model(X_test_ten).detach().numpy())
+    saveSubmission(predictions.flatten(), "NeuralNetwork_early_stopping")
