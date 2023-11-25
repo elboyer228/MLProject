aee99226dcc6e3288fb822b89dbffafde513c936
Johann Clausen <johann.clausen@epfl.ch>
2023-11-25 04:37:58 +0100

    Creating neuralNetworks() and selectFeatures() for easier feature selection and model analysis

--- Models/neuralNetworks.py
+++ Models/neuralNetworks.py
@@ -4,6 +4,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 from tqdm import tqdm
 
@@ -12,137 +13,188 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 import torch
 
-from tools import saveSubmission
+from tools import selectFeatures, getTarget, saveSubmission
 
 
 
-# Reproducibility
-seed_num = 2
-torch.use_deterministic_algorithms(True)
-random.seed(seed_num)
-np.random.seed(seed_num)
-torch.manual_seed(seed_num)
-def seed_worker(worker_id):
-    worker_seed = torch.initial_seed() % 2**32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
-g = torch.Generator()
-g.manual_seed(seed_num)
+def neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150, lr=0.0001, patience=50, verbose=True, export=False, name="NeuralNetwork"):
+    """
+    This function trains a basic neural network model on the given dataset.
+
+    Parameters
+    ----------
+    X_set : pandas.DataFrame or numpy.ndarray
+        The training dataset.
+    X_test : pandas.DataFrame or numpy.ndarray
+        The test dataset.
+    y_set : pandas.Series or numpy.ndarray
+        The target variable for the training dataset.
+    seed_num : int, optional
+        The seed for the random number generator, by default 2.
+    batch_size : int, optional
+        The batch size for training the model, by default 128.
+    epochs : int, optional
+        The number of epochs to train the model, by default 150.
+    lr : float, optional
+        The learning rate for the optimizer, by default 0.0001.
+    patience : int, optional
+        The patience for early stopping, by default 50.
+    verbose : bool, optional
+        If True, prints out the loss for each epoch, by default True.
+    export : bool, optional
+        If True, saves the model after training, by default False.
+    name : str, optional
+        The name of the model, by default "NeuralNetwork".
+
+    Returns
+    -------
+    None
+        This function doesn't return anything but prints out the training and validation loss.
+    """
+    # Reproducibility
+    torch.use_deterministic_algorithms(True)
+    random.seed(seed_num)
+    np.random.seed(seed_num)
+    torch.manual_seed(seed_num)
+    def seed_worker(worker_id):
+        worker_seed = torch.initial_seed() % 2**32
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
+    g = torch.Generator()
+    g.manual_seed(seed_num)
 
 
+    # Splitting test and train
 
-# Load data
-train = pd.read_csv("Features/train_properties.csv")
-test = pd.read_csv("Features/test_properties.csv")
-target = pd.read_csv("Data/train.csv")
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_set,
+        y_set,
+        test_size=0.20,
+        random_state=seed_num
+    )
 
-# Splitting test and train
+    # Standardize data
+    Xscaler = StandardScaler()
+    X_std = Xscaler.fit_transform(X_train)
+    X_val_std = Xscaler.transform(X_val)
+    X_test_std = Xscaler.transform(X_test)
+    Yscaler = StandardScaler()
+    y_std = Yscaler.fit_transform(y_train.values.reshape(-1, 1))
+    y_val_std = Yscaler.transform(y_val.values.reshape(-1, 1))
 
-X_train, X_val, y_train, y_val = train_test_split(
-    train.drop(["ID"], axis=1),
-    target["RT"],
-    test_size=0.2,
-    random_state=42
-)
+    # Convert data to pytorch tensors
+    X_ten, y_ten, X_val_ten, y_val_ten, X_test_ten = map(
+        lambda array: torch.tensor(array, dtype=torch.float32),
+        [X_std, y_std, X_val_std, y_val_std, X_test_std]
+    )
 
-# Standardize data
-Xscaler = StandardScaler()
-X_std = Xscaler.fit_transform(X_train)
-X_val_std = Xscaler.transform(X_val)
-X_test_std = Xscaler.transform(test.drop(["ID"], axis=1))
-Yscaler = StandardScaler()
-y_std = Yscaler.fit_transform(y_train.values.reshape(-1, 1))
-y_val_std = Yscaler.transform(y_val.values.reshape(-1, 1))
+    # Pytorch training data loader, for using batches
+    training_dataloader = torch.utils.data.DataLoader(
+        torch.utils.data.TensorDataset(X_ten, y_ten),
+        batch_size = batch_size,
+        shuffle = True,
+        worker_init_fn = seed_worker, # for reproducibility
+        generator = g # for reproducibility
+    )
 
-# Convert data to pytorch tensors
-X_ten, y_ten, X_val_ten, y_val_ten, X_test_ten = map(
-    lambda array: torch.tensor(array, dtype=torch.float32),
-    [X_std, y_std, X_val_std, y_val_std, X_test_std]
-)
-
-# Pytorch training data loader, for using batches
-training_dataloader = torch.utils.data.DataLoader(
-    torch.utils.data.TensorDataset(X_ten, y_ten),
-    batch_size = 128,
-    shuffle = True,
-    worker_init_fn = seed_worker, # for reproducibility
-    generator = g # for reproducibility
-)
-
-# Setting up the model
-class NeuralNet(torch.nn.Module):
-    def __init__(self):
-        super(NeuralNet, self).__init__()
-        self.layers = torch.nn.Sequential(
-            torch.nn.Linear(210, 128),
-            torch.nn.Dropout(0.5),
-            torch.nn.ReLU(),
-            torch.nn.Linear(128, 1),
-        )
-    def forward(self, x):
-        return self.layers(x)
-
-batch_size = 128
-epochs = 150
-
-# Initialize best validation loss as infinity
-best_val_loss = float('inf')
-# Initialize patience counter
-patience_counter = 0
-
-model = NeuralNet()
-loss_func = torch.nn.MSELoss()
-optimizer = torch.optim.AdamW(model.parameters())
-
-# Training
-model.train()
-for epoch in tqdm(range(epochs)):
-    model.train()
-    for data, target in training_dataloader:
-        optimizer.zero_grad()
-        pred = model(data)
-        loss = torch.sqrt(loss_func(pred, target))
-        loss.backward()
-        optimizer.step()
+    # Setting up the model
+    class NeuralNet(torch.nn.Module):
+        def __init__(self):
+            super(NeuralNet, self).__init__()
+            self.layers = torch.nn.Sequential(
+                torch.nn.Linear(X_set.shape[1], 128),
+                torch.nn.Dropout(0.5),
+                torch.nn.ReLU(),
+                torch.nn.Linear(128, 1),
+            )
+        def forward(self, x):
+            return self.layers(x)
         
 
-    # Validation
+    # Initialize best validation loss as infinity
+    best_val_loss = float('inf')
+    # Initialize patience counter
+    patience_counter = 0
+
+    model = NeuralNet()
+    loss_func = torch.nn.MSELoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    # Training
+    model.train()
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        for data, target in training_dataloader:
+            optimizer.zero_grad()
+            pred = model(data)
+            loss = torch.sqrt(loss_func(pred, target))
+            loss.backward()
+            optimizer.step()
+            
+        if verbose:
+            print(f'Epoch {epoch+1}/{epochs}', f'Loss: {loss.item()}', end='\r')
+            
+        # Validation
+        model.eval()
+        with torch.no_grad():
+            pred_val = model(X_val_ten)
+            val_loss = torch.sqrt(loss_func(pred_val, y_val_ten)) 
+            
+        # Check for early stopping
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            patience_counter = 0
+        else:
+            patience_counter += 1
+            if patience_counter >= patience:
+                print("Early stopping")
+                break
+
+
+    # Evaluation and computation of the train and test losss
     model.eval()
     with torch.no_grad():
-        pred_val = model(X_val_ten)  # assuming X_val_ten is your validation data
-        val_loss = torch.sqrt(loss_func(pred_val, y_val_ten))  # assuming y_val_ten is your validation target
+        pred_train = model(X_ten)
+        pred_val = model(X_val_ten)
 
-    # Check for early stopping
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        patience_counter = 0
-    else:
-        patience_counter += 1
-        if patience_counter >= 3:  # 5 is the patience
-            print("Early stopping")
-            break
+        # Inverse transform the predictions
+        pred_train = Yscaler.inverse_transform(pred_train.numpy())
+        pred_val = Yscaler.inverse_transform(pred_val.numpy())
+
+        # Convert the targets back to numpy and inverse transform
+        y_train_np = Yscaler.inverse_transform(y_ten.numpy())
+        y_val_np = Yscaler.inverse_transform(y_val_ten.numpy())
+
+        # Compute the loss with the inverse transformed data
+        train_loss = torch.sqrt(loss_func(torch.from_numpy(pred_train), torch.from_numpy(y_train_np)))
+        val_loss = torch.sqrt(loss_func(torch.from_numpy(pred_val), torch.from_numpy(y_val_np)))
+
+        print(f"Train loss: {train_loss}")
+        print(f"Validation loss: {val_loss}")
+        
+        if export:
+            predictions = Yscaler.inverse_transform(model(X_test_ten).detach().numpy())
+            saveSubmission(predictions.flatten(), name)
+            
+    return val_loss
 
 
-# Evaluation and computation of the train and test losss
-model.eval()
-with torch.no_grad():
-    pred_train = model(X_ten)
-    pred_val = model(X_val_ten)
 
-    # Inverse transform the predictions
-    pred_train = Yscaler.inverse_transform(pred_train.numpy())
-    pred_val = Yscaler.inverse_transform(pred_val.numpy())
 
-    # Convert the targets back to numpy and inverse transform
-    y_train_np = Yscaler.inverse_transform(y_ten.numpy())
-    y_val_np = Yscaler.inverse_transform(y_val_ten.numpy())
 
-    # Compute the loss with the inverse transformed data
-    train_loss = torch.sqrt(loss_func(torch.from_numpy(pred_train), torch.from_numpy(y_train_np)))
-    test_loss = torch.sqrt(loss_func(torch.from_numpy(pred_val), torch.from_numpy(y_val_np)))
+X_set, X_test = selectFeatures(Lab=True, mol=True) 
+y_set = getTarget()
 
-    print(f"Train loss: {train_loss}")
-    print(f"Validation loss: {test_loss}")
+# neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150, lr=0.0001, patience=50, verbose=False, export=False, name="NeuralNetwork")
+
+# Plot different learning rates
+learning_rates = np.linspace(0.00001, 0.001, 10)
+validation_losses = []
+for lr in learning_rates:
+    validation_losses.append(neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=1500, lr=lr, patience=50, verbose=False, export=False, name="NeuralNetwork"))
     
-    predictions = Yscaler.inverse_transform(model(X_test_ten).detach().numpy())
-    saveSubmission(predictions.flatten(), "NeuralNetwork_early_stopping")
+plt.plot(learning_rates, validation_losses)
+plt.xlabel("Learning rate")
+plt.ylabel("Validation loss")
+plt.xscale("log")
+plt.savefig("Analysis/NeuralNetworks/learning_rates.png")
--- tools.py
+++ tools.py
@@ -8,11 +8,6 @@ from rdkit.Chem import Descriptors
 
 
 
-# Import the data
-train = pd.read_csv("Data/train.csv")
-test = pd.read_csv("Data/test.csv")
-
-
 def molPropToCSV(df, name):
     """This function calculates molecular properties for each molecule in the given DataFrame and saves them to a CSV file.
 
@@ -40,3 +35,69 @@ def saveSubmission(predicted, name = "output"):
     """
     submission = pd.DataFrame({'ID': range(1, len(predicted)+1) , 'RT': predicted})
     submission.to_csv("Submissions/"+name+".csv", index=False)
+    
+    
+def selectFeatures(Lab=False, ECFP=False, cddd=False, mol=False):
+    """
+    This function selects the specified features from the training and test datasets.
+    The features are extracted from the enhanced dataset contained in the Data folder.
+    If no features are specified, an empty DataFrame is returned.
+
+    Parameters
+    ----------
+    Lab : bool, optional
+        If True, includes features 'Lab_1' to 'Lab_24'. By default, False.
+    ECFP : bool, optional
+        If True, includes features 'ECFP_1' to 'ECFP_1024'. By default, False.
+    cddd : bool, optional
+        If True, includes features 'cddd_1' to 'cddd_512'. By default, False.
+    mol : bool, optional
+        If True, includes features 'MaxAbsEStateIndex' to 'fr_urea'. By default, False.
+
+    Returns
+    -------
+    tuple of pandas.DataFrame
+        The first DataFrame contains the selected features from the training dataset.
+        The second DataFrame contains the selected features from the test dataset.
+    """
+    
+    train = pd.read_csv("Data/full_train_data.csv")
+    test = pd.read_csv("Data/full_test_data.csv")
+        
+    train_features = []
+    test_features = []
+        
+    if Lab:
+        Lab_train = train.loc[:, 'Lab_1':'Lab_24']
+        train_features.append(Lab_train)
+        Lab_test = test.loc[:, 'Lab_1':'Lab_24']
+        test_features.append(Lab_test)
+    if ECFP:
+        ECFP_train = train.loc[:, 'ECFP_1':'ECFP_1024']
+        train_features.append(ECFP_train)
+        ECFP_test = test.loc[:, 'ECFP_1':'ECFP_1024']
+        test_features.append(ECFP_test)
+    if cddd:
+        cddd_train = train.loc[:, 'cddd_1':'cddd_512']
+        train_features.append(cddd_train)
+        cddd_test = test.loc[:, 'cddd_1':'cddd_512']
+        test_features.append(cddd_test)
+    if mol:
+        molecular_train = train.loc[:, 'MaxAbsEStateIndex':'fr_urea']
+        train_features.append(molecular_train)
+        molecular_test = test.loc[:, 'MaxAbsEStateIndex':'fr_urea']
+        test_features.append(molecular_test)
+    
+    return pd.concat(train_features, axis=1), pd.concat(test_features, axis=1)
+
+
+def getTarget():
+    """This function returns the target variable from the training dataset.
+
+    Returns
+    -------
+    pandas.Series
+        The target variable.
+    """
+    train = pd.read_csv("Data/full_train_data.csv")
+    return train.loc[:, 'RT']
