0096d881fcbda9582dcf1e790c967355bd991bb9
elboyer228 <elise.boyer@epfl.ch>
2023-11-27 13:10:31 +0100

    Final ? Cross validation on neural netwrok

--- Models/neuralNetworks_CV.py
+++ Models/neuralNetworks_CV.py
@@ -1,3 +1,6 @@
+
+
+
 # Including parent folder for imports
 import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
@@ -13,13 +16,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 import torch
-from torch.utils.data import DataLoader, SubsetRandomSampler
 
-from tools import selectFeatures, getTarget, saveSubmission
-
+from tools import selectFeatures, getTarget, plotHistory, saveSubmission
 
 
-def neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150, lr=0.0001, patience=50, verbose=True, export=False, name="NeuralNetwork", k_fold=5):
+
+def neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150, lr=0.0001, patience=50, verbose=True, export=False, name="NeuralNetwork", k_fold = 5):
     """
     This function trains a basic neural network model on the given dataset.
 
@@ -67,10 +69,13 @@ def neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150,
 
 
     def reset_weights(m):
+        """
+        Try to reset the weights of the model
+        """
         for layer in m.children():
             if hasattr(layer, 'reset_parameters'):
-             print(f'Reset trainable parameters of layer = {layer}')
-             layer.reset_parameters()
+                print(f'Reset trainable parameters of layer = {layer}')
+                layer.reset_parameters()
 
 
     # Setting up the model
@@ -85,139 +90,142 @@ def neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150,
             )
         def forward(self, x):
             return self.layers(x)
-
-
-    model = NeuralNet()
-    model.apply(reset_weights)
-
-    best_val_loss = float('inf')
-    patience_counter = 0
-
+    
+    
+    
     loss_func = torch.nn.MSELoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    train_losses = []
+    val_losses = []
 
-    kf = KFold(n_splits= k_fold, shuffle=True, random_state=seed_num)
+    kfold = KFold(n_splits=k_fold, shuffle=True, random_state=seed_num)
+    print('-' * 50)
 
-     
-    for fold, (train_index, val_index) in enumerate(kf.split(X_set)):
-        print(f'FOLD {fold}')
-        print('--------------------------------')
-        train_sampling = SubsetRandomSampler(train_index)
-        test_sampling = SubsetRandomSampler(val_index)
+    for fold, (train_index, val_index) in enumerate(kfold.split(X_set, y_set)):
+        print(f'Fold {fold + 1 }')
+        print('-' * 50)
 
-        X_train_datas = DataLoader(X_set, batch_size=batch_size, sampler=train_sampling)
-        X_val_datas = test_sampling.utils.data.DataLoader(X_set, batch_size=batch_size, sampler=test_sampling)
-        # avec les y_train et y_val dedans 
+        X_train, X_val = X_set.iloc[train_index], X_set.iloc[val_index]
+        y_train, y_val = y_set.iloc[train_index], y_set.iloc[val_index]
+        
+        model = NeuralNet()
+        model.apply(reset_weights)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+        best_val_loss = float('inf')
+        patience_counter = 0
 
+        # Standardize data
+        Xscaler = StandardScaler()
+        X_std = Xscaler.fit_transform(X_train)
+        X_val_std = Xscaler.transform(X_val)
+        X_test_std = Xscaler.transform(X_test)
+        Yscaler = StandardScaler()
+        y_std = Yscaler.fit_transform(y_train.values.reshape(-1, 1))
+        y_val_std = Yscaler.transform(y_val.values.reshape(-1, 1))
 
-# training part 
-        for batch_train in X_train_datas:
-            X_train, y_train = batch_train
-
-            Xscaler = StandardScaler()
-            X_std = Xscaler.fit_transform(X_train)
-            Yscaler = StandardScaler()
-            y_std = Yscaler.fit_transform(y_train.values.reshape(-1, 1))
-            
-            X_ten, y_ten = map(
-                lambda array: torch.tensor(array, dtype=torch.float32),
-                [X_std, y_std]
+        # Convert data to pytorch tensors
+        X_ten, y_ten, X_val_ten, y_val_ten, X_test_ten = map(
+            lambda array: torch.tensor(array, dtype=torch.float32),
+            [X_std, y_std, X_val_std, y_val_std, X_test_std]
             )
 
-            training_dataloader = torch.utils.data.DataLoader(
-                torch.utils.data.TensorDataset(X_ten, y_ten),
-                batch_size = batch_size,
-                shuffle = True,
-                worker_init_fn = seed_worker, # for reproducibility
-                generator = g # for reproducibility
+        # Pytorch training data loader, for using batches
+        training_dataloader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(X_ten, y_ten),
+            batch_size = batch_size,
+            shuffle = True,
+            worker_init_fn = seed_worker, # for reproducibility
+            generator = g # for reproducibility
             )
 
+        # Training
+        model.train()
+        iterator = tqdm(range(epochs)) if not verbose else range(epochs)
+        for epoch in iterator:
             model.train()
-            for epoch in tqdm(range(epochs)):
-                for data, target in training_dataloader:
-                    optimizer.zero_grad()
-                    pred = model(data)
-                    loss = torch.sqrt(loss_func(pred, target))
-                    loss.backward()
-                    optimizer.step()
-            
-                if verbose:
-                    print(f'Epoch {epoch+1}/{epochs}', f'Loss: {loss.item()}', end='\r')
+            for data, target in training_dataloader:
+                optimizer.zero_grad()
+                pred = model(data)
+                loss = (loss_func(pred, target)) # RMSE --> MSE
+                loss.backward()
+                optimizer.step()
                 
             # Validation
-                model.eval()
-                with torch.no_grad():
-                    pred_val = model(X_val_ten)
-                    val_loss = torch.sqrt(loss_func(pred_val, y_val_ten)) 
-                    
-                # Check for early stopping
-                if val_loss < best_val_loss:
-                    best_val_loss = val_loss
-                    patience_counter = 0
-                else:
-                    patience_counter += 1
-                    if patience_counter >= patience:
-                        print("Early stopping")
-                        break
-
-# validation part
-        for batch_val in X_val_datas:
-            X_val, y_val = batch_val
-
-            Xscaler = StandardScaler()
-            X_val_std = Xscaler.transform(X_val)
-            Yscaler = StandardScaler()
-            y_val_std = Yscaler.transform(y_val.values.reshape(-1, 1))
-
-            X_val_ten, y_val_ten = map(
-                lambda array: torch.tensor(array, dtype=torch.float32),
-                [X_val_std, y_val_std]
-            )
-
             model.eval()
             with torch.no_grad():
-                pred_train = model(X_ten)
                 pred_val = model(X_val_ten)
-
-                # Inverse transform the predictions
-                pred_train = Yscaler.inverse_transform(pred_train.numpy())
-                pred_val = Yscaler.inverse_transform(pred_val.numpy())
-
-                # Convert the targets back to numpy and inverse transform
-                y_train_np = Yscaler.inverse_transform(y_ten.numpy())
-                y_val_np = Yscaler.inverse_transform(y_val_ten.numpy())
-
-                # Compute the loss with the inverse transformed data
-                train_loss = torch.sqrt(loss_func(torch.from_numpy(pred_train), torch.from_numpy(y_train_np)))
-                val_loss = torch.sqrt(loss_func(torch.from_numpy(pred_val), torch.from_numpy(y_val_np)))
-
-                print(f"Train loss: {train_loss}")
-                print(f"Validation loss: {val_loss}")
+                val_loss = (loss_func(pred_val, y_val_ten)).numpy()
                 
-                # if export:
-                #     predictions = Yscaler.inverse_transform(model(X_test_ten).detach().numpy())
-                #     saveSubmission(predictions.flatten(), name)
+            if verbose and epoch % 10 == 0:
+                print(f'Epoch {epoch}/{epochs} ({round((epoch/epochs)*100, 1)}%)', f'Training Loss: {loss.item()}', f"Validation loss : {val_loss}", end='\r')
+                
+            # Check for early stopping
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                patience_counter = 0
+            else:
+                patience_counter += 1
+                if patience_counter >= patience:
+                    if not verbose:
+                        iterator.close()
+                    print("\n ----- Early stopping -----")
+                    break
+
+
+        # Evaluation and computation of the train and test losss
+        model.eval()
+        with torch.no_grad():
             
+            pred_train = model(X_ten).detach()
+            pred_val = model(X_val_ten).detach()
+
+            # Inverse transform the predictions
+            pred_train = Yscaler.inverse_transform(pred_train.numpy())
+            pred_val = Yscaler.inverse_transform(pred_val.numpy())
+            y_train = Yscaler.inverse_transform(y_ten.numpy())
+            y_val = Yscaler.inverse_transform(y_val_ten.numpy())
+
+            # Compute the loss with the inverse transformed data
+            train_loss = loss_func(torch.from_numpy(pred_train), torch.from_numpy(y_train))
+            val_loss = loss_func(torch.from_numpy(pred_val), torch.from_numpy(y_val))
+
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            print('Train loss and validation loss after inverting the predictions')
+            print(f"Train loss: {train_loss}, Validation loss: {val_loss}")
+
             
-    return val_loss
+    model.eval()
+    with torch.no_grad():
+        predictions = Yscaler.inverse_transform(model(X_test_ten).detach().numpy())
+        if export:
+            saveSubmission(predictions.flatten(), name)   
 
-
-
+    return train_losses, val_losses
 
 
 X_set, X_test = selectFeatures(Lab=True, mol=True) 
 y_set = getTarget()
 
-neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=150, lr=0.0001, patience=50, verbose=False, export=False, name="NeuralNetwork")
+history, train_losses, val_losses = neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=1500, lr=0.00023, patience=50, verbose=False, export=False, name="NeuralNetwork_LabMol_CV")
 
-# # Plot different learning rates
-# learning_rates = np.linspace(0.00001, 0.001, 10)
-# validation_losses = []
-# for lr in learning_rates:
-#     validation_losses.append(neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=1500, lr=lr, patience=50, verbose=False, export=False, name="NeuralNetwork"))
-    
-# plt.plot(learning_rates, validation_losses)
-# plt.xlabel("Learning rate")
-# plt.ylabel("Validation loss")
-# plt.xscale("log")
-# plt.savefig("Analysis/NeuralNetworks/learning_rates.png")
+print(f"Average train loss unstanderdize : {sum(train_losses) / len(train_losses)}")
+print(f"Average validation loss unstandardize : {sum(val_losses) / len(val_losses)}")
+
+# Plot different learning rates
+def plotLR():
+    learning_rates = np.linspace(0.00001, 0.001, 10)
+    train_losses = []
+    validation_losses = []
+    for lr in learning_rates:
+        train_loss, validation_loss = neuralNetworks(X_set, X_test, y_set, seed_num=2, batch_size=128, epochs=1500, lr=lr, patience=50, verbose=False, export=False, name="NeuralNetwork")
+        train_losses.append(train_loss)
+        validation_losses.append(validation_loss)
+        print(f"Learning rate: {lr}, Train loss : {train_losses[-1]}, Validation loss: {validation_losses[-1]}")
+        
+    plt.plot(learning_rates, validation_losses)
+    plt.xlabel("Learning rate")
+    plt.ylabel("Validation loss")
+    plt.xscale("log")
+    plt.show()
+
+
